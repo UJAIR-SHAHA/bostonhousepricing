cd1a93ed239c104bcdb3d9e3d97e11c950418e9f
UJAIR_SHAHA <shahaujair@gmail.com>
2024-05-18 16:48:33 +0530

    added home page and flask

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ def home():
 
 
 @app.route('/Predict_api',methods = ['POST'])
-
 def predict_api():
     data = request.json['data']
     print(data)
@@ -25,6 +24,14 @@ def predict_api():
     output = model.predict(new_data)
     print[output[0]]
     return jsonify[output[0]]
+
+@app.route('/Predict',methods = ['POST'])
+def predict():
+    data = [float (x) for x in request.form.values()]
+    final_input = scalar.transform((np.array(list(data)).reshape(1,-1)))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template('home.html',prediction_text= "the predicted house price is {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
